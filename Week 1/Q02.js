b92b38b205b512302bd05c54c14e360b83317066
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-05 09:54:10 +0530

    concatenation

---
+++ Week 1/Q02.js
@@ -0,0 +1,32 @@
+// string + string = string (concatenation)
+
+let a = "Hello"
+let b = " world"
+
+console.log(a+b);
+
+
+// string + number = string (concatenation)
+
+let c = "Hello "
+let d = 10
+
+console.log(c+d);
+
+
+//typeof
+
+let c = "Hello "
+let d = 10
+
+console.log(typeof(c+d));
+
+
+// one tricky question
+
+let e = 10
+let f = 20
+
+console.log("sum of " + e + " and " + f + " is " + (e + f));
+console.log(e + f +" is the sum of " + e + " and " + f );
+
