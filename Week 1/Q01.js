a2a8d21bf1b885d497e794f78a19ceb1f5df12b4
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-05 09:14:52 +0530

    sum of two integers

---
+++ Week 1/Q01.js
@@ -0,0 +1,18 @@
+// sum of two integers
+
+
+let a = 10;
+let b = 20;
+
+console.log(a+b);
+
+
+
+
+//Another way to sum two integers
+let c = 30;
+let d = 40;
+
+let sum = c+d;
+
+console.log(sum);
