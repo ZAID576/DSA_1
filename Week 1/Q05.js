7da81ac20e2bf3675dd399c586117bf9c7cfc2ac
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-05 11:29:21 +0530

    3nd way of swapping two variables using destructuring

--- Week 1/Q05.js
+++ Week 1/Q05.js
@@ -1,14 +1,14 @@
 // Swapping two variable 1st way
 
-let a = 10
-let b = 20
+// let a = 10
+// let b = 20
 
-let c = a
-a = b
-b = c
+// let c = a
+// a = b
+// b = c
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
 
 // output = 20 10
 
@@ -16,13 +16,26 @@ console.log(b);
 
 //2nd way of swapping two variables
 
-let a = 10
-let b = 20
+// let a = 10
+// let b = 20
 
-a = a + b
-b = a - b
-a = a - b
+// a = a + b
+// b = a - b
+// a = a - b
 
-console.log(a,b);
+// console.log(a,b);
+
+// output 20 10
+
+
+
+//3nd way of swapping two variables using destructuring
+
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+
+console.log(a, b);
 
 // output 20 10
