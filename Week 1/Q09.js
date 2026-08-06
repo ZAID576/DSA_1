2da9ffe0e1774c509f08a8c6f92b35a502fe35a0
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-06 16:10:15 +0530

    OTP Generation

---
+++ Week 1/Q09.js
@@ -0,0 +1,8 @@
+// OTP Generation 
+
+console.log((Math.random() * 9999));
+console.log(Math.trunc(Math.random() * 9999)+1000);
+ 
+// multiplying it by 9999 means it will never generate a OTP above 9999 & adding 1000 means it will always generate OTP above 1000
+// above 1000 and below 9999
+
