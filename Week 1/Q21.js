d7aa3223a761ec33e4fadd6ad65eb12da5c5fb51
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-09 12:12:02 +0530

    Loops - Accept an integer from user and print n times

---
+++ Week 1/Q21.js
@@ -0,0 +1,8 @@
+// Loops - Accept an integer from user and print n times
+
+let prompt = require("prompt-sync")()
+let n = prompt("Enter your number = ")
+
+for(let i=0; i<=n; i++){
+   console.log("Hello Guys");  
+}
