2d60d1acedf097a61c662c4f7af8e50836fbe04c
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-10 23:39:51 +0530

    Loops - Print all the factors of the numbers

---
+++ Week 1/Q27.js
@@ -0,0 +1,13 @@
+// Loops - Print all the factors of the numbers
+
+let prompt = require("prompt-sync")();
+
+let n = prompt("Enter a number = ");
+
+console.log("Factors are:");
+
+for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+        console.log(i);
+    }
+}
