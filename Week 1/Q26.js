cb2c701bed9155a9defad9df63535f8afe60923a
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-10 23:33:21 +0530

    Loops - Print the sum of all even & odd numbers in a range separately

--- Week 1/Q26.js
+++ Week 1/Q26.js
@@ -1,22 +1,2 @@
 // Loops - Print the sum of all even & odd numbers in a range separately
 
-let prompt = require("prompt-sync")();
-
-let start = prompt("Enter starting number = ");
-let end = prompt("Enter ending number = ");
-
-let evenSum = 0;
-let oddSum = 0;
-
-for (let i = start; i <= end; i++) {
-
-    if (i % 2 == 0) {
-        evenSum = evenSum + i;
-    }
-    else {
-        oddSum = oddSum + i;
-    }
-}
-
-console.log("Sum of even numbers =", evenSum);
-console.log("Sum of odd numbers =", oddSum);
